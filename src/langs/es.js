fa055f73b407c784e0e367050e0b3476e8d10424
José María Muntané <munta@muntane.es>
2024-01-03 13:07:52 +0100

    spanish translation
    Spanish translations

---
+++ src/langs/es.js
@@ -0,0 +1,17 @@
+//Add strings
+gdprCookieNoticeLocales.es = {
+  description: 'Utilizamos cookies para ofrecerle una mejor experiencia de navegación, personalizar contenidos y analizar nuestro tráfico. Lea cómo utilizamos las cookies y cómo puede controlarlas haciendo clic Ajustes Cooklies.',
+  settings: 'Ajustar cookies',
+  accept: 'Aceptar cookies',
+  statement: 'Declaración sobre cookies',
+  save: 'Guardar ajustes',
+  always_on: 'Aceptar todas',
+  cookie_essential_title: 'Cookies esenciales del sitio web',
+  cookie_essential_desc: 'Las cookies necesarias ayudan a que un sitio web sea utilizable al permitir funciones básicas como la navegación por páginas y el acceso a áreas seguras del sitio web. El sitio web no puede funcionar correctamente sin estas cookies.',
+  cookie_performance_title: 'Cookies de rendimiento',
+  cookie_performance_desc: 'Estas cookies se utilizan para mejorar el rendimiento y la funcionalidad de nuestros sitios web, pero no son esenciales para su uso. Por ejemplo, almacenan su idioma preferido o la región en la que se encuentra.',
+  cookie_analytics_title: 'Cookies de analítica',
+  cookie_analytics_desc: 'Utilizamos cookies analíticas para ayudarnos a medir cómo interactúan los usuarios con el contenido del sitio web, lo que nos ayuda a personalizar nuestros sitios web y la aplicación para usted con el fin de mejorar su experiencia.',
+  cookie_marketing_title: 'Cookies de marketing',
+  cookie_marketing_desc: 'Estas cookies se utilizan para que los mensajes publicitarios sean más relevantes para usted y sus intereses. La intención es mostrar anuncios que sean relevantes y atractivos para el usuario individual y, por lo tanto, más valiosos para los editores y los anunciantes de terceros..'
+}
